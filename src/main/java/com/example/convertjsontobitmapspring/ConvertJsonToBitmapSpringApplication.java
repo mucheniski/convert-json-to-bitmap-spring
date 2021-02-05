7f0193cd8bbd691d1e4c9b9e33708c44483e1886
mucheniski <mucheniski@gmail.com>
2021-02-05 09:48:38 -0300

    First Commit

---
+++ src/main/java/com/example/convertjsontobitmapspring/ConvertJsonToBitmapSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.convertjsontobitmapspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertJsonToBitmapSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertJsonToBitmapSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/convertjsontobitmapspring/ConvertJsonToBitmapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertjsontobitmapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertJsonToBitmapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
