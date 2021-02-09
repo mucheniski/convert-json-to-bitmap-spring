581341a2e433b6f24f35d67fd879bc4921edc4fb
mucheniski <mucheniski@gmail.com>
2021-02-09 11:03:32 -0300

    Page configuration with width and heigth

--- src/main/java/com/example/convertjsontobitmapspring/common/HTMLConverter.java
+++ src/main/java/com/example/convertjsontobitmapspring/common/HTMLConverter.java
@@ -3,12 +3,16 @@ package com.example.convertjsontobitmapspring.common;
 import com.example.convertjsontobitmapspring.domain.enums.FileType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.aspose.html.HTMLDocument;
 import com.aspose.html.saving.ImageSaveOptions;
 import com.aspose.html.rendering.image.ImageFormat;
 import com.aspose.html.converters.Converter;
-import org.springframework.web.servlet.ModelAndView;
+import com.aspose.html.rendering.PageSetup;
+import com.aspose.html.drawing.Page;
+import com.aspose.html.drawing.Size;
+import com.aspose.html.drawing.Length;
 
 import javax.validation.Valid;
 
@@ -22,12 +26,24 @@ public class HTMLConverter {
     public void convertHtmlToBitmap(ModelAndView statementPage) {
         String viewNameHtml = addHtmlExtension(statementPage.getViewName());
         HTMLDocument document = new HTMLDocument(htmlConverterDefaultAddress + viewNameHtml);
+        PageSetup pageSetup = configurePage(98, 512);
         ImageSaveOptions options = new ImageSaveOptions(ImageFormat.Bmp);
+        options.setPageSetup(pageSetup);
+
         Converter.convertHTML(document, options,htmlConverterDefaultAddress + "output.bmp");
         if (document != null) {
             document.dispose();
         }
 
+    }
+
+    private PageSetup configurePage(int pageWidth, int pageHeigth) {
+        PageSetup pageSetup = new PageSetup();
+        Page page = new Page();
+        Size pageSize = new Size(Length.fromPixels(pageWidth), Length.fromPixels(pageHeigth));
+        page.setSize(pageSize);
+        pageSetup.setAnyPage(page);
+        return pageSetup;
     }
 
     private String addHtmlExtension(String viewName) {
