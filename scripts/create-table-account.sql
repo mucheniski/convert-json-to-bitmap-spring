5656c2282c1f09c16b85caed6349408551c1db93
mucheniski <mucheniski@gmail.com>
2021-02-24 16:53:50 -0300

    Added fields on account for tests in HTML page

--- scripts/create-table-account.sql
+++ scripts/create-table-account.sql
@@ -1,9 +1,9 @@
-CREATE TABLE account (
-	id BIGINT auto_increment NOT NULL,
-	name varchar(60) NOT NULL,	
-	balance DECIMAL(10,2) NOT NULL,
-	CONSTRAINT Cozinha_PK PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci;
+CREATE TABLE `account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `bankName` varchar(60) NOT NULL,
+  `agency` varchar(60) NOT NULL,
+  `number` varchar(60) NOT NULL,
+  `balance` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
